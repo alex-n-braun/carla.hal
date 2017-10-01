bf6745f67756efdbaa886654212b4e9b7686d4f2
daniel <dyh.1213@gmail.com>
2017-10-01 08:27:47 -0700

    changed the way the car acts during green to red TL change

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -89,7 +89,14 @@ class TLDetector(object):
         of times till we start using it. Otherwise the previous stable state is
         used.
         '''
-        if self.state != state:
+	logItem = "RED" if state == TrafficLight.RED else "GREEN"
+        rospy.logwarn("TLSTATE: " + logItem)
+	if state == TrafficLight.GREEN:
+	    self.last_state = self.state
+            light_wp = -1
+            self.last_wp = light_wp
+            self.upcoming_red_light_pub.publish(Int32(light_wp))
+        elif self.state != state:
             self.state_count = 0
             self.state = state
         elif self.state_count >= STATE_COUNT_THRESHOLD:
